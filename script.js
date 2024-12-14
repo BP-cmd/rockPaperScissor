e98e314703921a77232821a66271106241f00409
BP-cmd <bhaswadip@gmail.com>
2024-12-15 02:16:50 +0530

    added game play function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Welcome to the game!!")
+console.log("Welcome to the game!!");
 // let userChoice = prompt("Choose your move -- rock/paper/scissors ?", null)
 
 //function to create random number between 0,1,2 each time it's called
@@ -13,3 +13,17 @@ let computerMove = ()=>{
     case 2: return "paper";
   }
 };
+
+let playGame = (Player)=>{
+  let player = Player.toLowerCase();
+  let computer = computerMove()
+  if((player==='rock' && computer==='scissors')||(player==='scissors' && computer==='paper')||(player==='paper' && computer==='rock')){
+    alert("Player wins");
+  } else if ((player==='rock' && computer==='paper')||(player==='scissors' && computer==='rock')||(player==='paper' && computer==='scissors')){
+    alert("Computer wins");
+  } else if (player===computer){
+    alert("Game Draw");
+  } else {
+    alert("Choose a valid move i.e rock, paper, scissors");
+  }
+}
