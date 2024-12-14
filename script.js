a3bcec298d9ea9ea22ba5b929f431ba399c6f4e2
BP-cmd <bhaswadip@gmail.com>
2024-12-15 01:53:35 +0530

    added computer move

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 console.log("Welcome to the game!!")
-let userChoice = prompt("please enter your name", null)
+// let userChoice = prompt("Choose your move -- rock/paper/scissors ?", null)
+
+//function to create random number between 0,1,2 each time it's called
+let randomNum = ()=>{
+  return Math.floor(Math.random()*100)%3;
+};
+//function evaluate get computer move
+let computerMove = ()=>{
+  switch(randomNum()){
+    case 0: return "rock";
+    case 1: return "scissors";
+    case 2: return "paper";
+  }
+};
